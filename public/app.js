0253cfabfae92b57cfeb7b11806dd779baf15988
Tobsi-385 <tobias.silva@gmx.de>
2025-11-06 11:50:49 +0100

    Reduce dice roll timeout from 5s to 1s

--- public/app.js
+++ public/app.js
@@ -39,7 +39,7 @@ function rollDice() {
         diceElements.forEach(die => {
             die.classList.remove('rolling');
         });
-    }, 5000);
+    }, 1000);
 }
 
 function selectScore(cat) {
